ab9e300d6d9320aac182fadbc26044d3b0a80dbc
Parker Miller <pmiller19@huskers.unl.edu>
2021-03-04 16:17:51 -0600

    made navBar look nicer

--- scheduler-frontend/src/components/navBar.js
+++ scheduler-frontend/src/components/navBar.js
@@ -4,7 +4,7 @@ export class navBar extends Component {
 
     render() {
         return (
-            <div className="bg-coolGrey flex pt-5 pb-5 text-white text-2xl">
+            <div className="bg-coolGrey flex pt-5 pb-5 text-white text-2xl mb-12">
                 <button className="mr-auto ml-auto">EVENT HOME</button>
                 <button className="mr-auto ml-auto">QUESTIONAIRE RESPONSE</button>
                 <button className="mr-auto ml-auto">PROFILE</button>
