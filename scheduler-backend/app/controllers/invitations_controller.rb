dfc71126c39c0497c3f68f5037f4ae3e20168b60
lseper <lseper2@huskers.unl.edu>
2021-03-30 16:27:25 -0500

    Initial Invitation Action setup

--- scheduler-backend/app/controllers/invitations_controller.rb
+++ scheduler-backend/app/controllers/invitations_controller.rb
@@ -1,15 +1,22 @@
 class InvitationsController < ApplicationController
+    before_action :set_invitation, only: [:show, :update, :destroy]
+    before_action :authorized, only: [:index, :create, :destroy, :show, :update]
     # Controller for Invitations
     
     # GET /user/:id/invitations
     def index
-    @invitations = invitation.all
-    render json: @invitations
+    @invitations = User.find(params[:user_id]).invitations
+    invited_events = []
+
+    for invitation in @invitations
+        invited_events.append(extract_invitation_info(invitation))
+    end
+    render json: invited_events
     end
 
-    # GET /invitations/:id
+    # GET user/:id/invitations/:id
     def show
-        render json: {message: "Success"}
+        render json: extract_invitation_info(@invitation)
     end
 
     # POST /user/:id/invitations
@@ -37,13 +44,23 @@ class InvitationsController < ApplicationController
     end
 
     private
+
+    def extract_invitation_info(invite)
+        event = Event.find(invite[:event_id])
+        organizer = User.find(event[:user_id])
+        {
+            organizer: organizer,
+            event_details: event
+        }
+    end
+
     # Use callbacks to share common setup or constraints between actions.
     def set_invitation
-        @invitation = invitation.find(params[:id])
+        @invitation = Invitation.find(params[:id])
     end
 
     # Only allow a list of trusted parameters through.
     def invitation_params
-        params.require(:invitation).permit(:event_id, )
+        params.require(:invitation).permit(:event_id, :user_id)
     end
 end
