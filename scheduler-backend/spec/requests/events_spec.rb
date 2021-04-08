d63853514e4c666b51ae83ca723beb196395bd61
lseper <lseper2@huskers.unl.edu>
2021-04-08 17:18:15 -0500

    initial event testing examples done with authorization for index action. Also added simple field presence validation for events in the database

--- scheduler-backend/spec/requests/events_spec.rb
+++ scheduler-backend/spec/requests/events_spec.rb
@@ -1,12 +1,23 @@
 require 'rails_helper'
 
 RSpec.describe "events requests", type: :request do
+  before(:context) do 
+    post '/login', params: { username: "testuser", password: "password" }
+    # token for user_id = 1
+    @token = JSON.parse(response.body)["auth_token"]
+  end
   describe "GET /index" do
 
     it "is unauthorized when the user requesting the events for this user" do
-      get '/users/10/events'
-
+      get '/users/3/events', headers: {'Authorization' => 'Bearer ' + @token}
       expect(response).to have_http_status(:unauthorized)
     end
+
+    it "is authorized when the user requesting the events for themself" do
+      get '/users/1/events', headers: {'Authorization' => 'Bearer ' + @token}
+      expect(response).to have_http_status(:ok)
+    end
+
+    it
   end
 end
