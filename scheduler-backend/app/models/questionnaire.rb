7cc0f3e9f4edaf650adcc4ef01639422fef2869e
lseper <lseper2@huskers.unl.edu>
2021-04-22 17:07:58 -0500

    password reset route renamed to '/reset_password'

--- scheduler-backend/app/models/questionnaire.rb
+++ scheduler-backend/app/models/questionnaire.rb
@@ -1,3 +1,14 @@
 class Questionnaire < ApplicationRecord
     belongs_to :user
+
+    # outside?
+    # inside large?
+    # inside small?
+    # online?
+    # eating?
+    # social distancing importance?
+    # mask importance?
+    # people around tolerance?
+
+    validates :q1answer, :q2answer, :q3answer, :q4answer, :q5answer, :q6answer, :q7answer, :q8answer, presence: true
 end
